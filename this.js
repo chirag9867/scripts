24fa4d647ed1f0c75773edcd79076eab727bf325
Chirag Mahajan <49065730+chirag9867@users.noreply.github.com>
2026-04-17 05:01:54 +0530

    Update this.js

--- this.js
+++ this.js
@@ -66,3 +66,24 @@ greet.call(user);          // "Hello, Bob" — calls immediately
 greet.apply(user);         // "Hello, Bob" — calls immediately
 const bound = greet.bind(user);
 bound();                   // "Hello, Bob" — returns a new function
+
+
+//Setters and Getters
+const person = {
+    names1: 'Alice',
+
+    get name(){
+        return this.names1;
+    },
+
+    set name(value){
+        this.names1 = value;
+    }
+};
+
+console.log(person.name);
+person.name = 'chirag';
+console.log(person.name);
+
+
+
